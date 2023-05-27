54e495049172e81066014ad8f70091d5884abf6c
Ubuntu <ubuntu@ip-172-31-14-56.us-east-2.compute.internal>
2023-05-27 05:24:25 +0000

    small kmain function implemented

--- bootloader/loader.s
+++ bootloader/loader.s
@@ -5,7 +5,9 @@ FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
 
-section .text:                  ; start of the text (code) section
+KERNEL_STACK_SIZE equ 4096	; size of stack in bytes
+
+section .text                   ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
     dd FLAGS                    ; the flags,
@@ -13,5 +15,16 @@ align 4                         ; the code must be 4 byte aligned
 
 loader:                         ; the loader label (defined as entry point in linker script)
     mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+					                            ; stack 
+    extern kmain                ; function is defined elsewhere
+    call kmain                  ; result of function will live in eax register
 .loop:
     jmp .loop                   ; loop forever
+
+
+section .bss
+align 4				; align at 4 bytes
+kernel_stack:			; label points to beginning of memory
+	resb KERNEL_STACK_SIZE  ; reserve stack for the kernel
+

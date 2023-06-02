92f8a69e62179be53a84ace400fe539228469e8e
Ubuntu <ubuntu@ip-172-31-14-56.us-east-2.compute.internal>
2023-06-02 00:02:57 +0000

    working framebuffer

---
+++ bootloader/fb.c
@@ -0,0 +1,62 @@
+#include "io.h"
+
+/* The I/O ports */
+#define FB_COMMAND_PORT 0x3D4
+#define FB_DATA_PORT 0x3D5
+
+/* The I/O port commands */
+#define FB_HIGH_BYTE_COMMAND 14
+#define FB_LOW_BYTE_COMMAND 15
+
+/* framebuffer memory */
+char *fb = (char *) 0x000B8000;
+
+/* colours */
+#define FB_GREEN     2
+#define FB_DARK_GREY 8
+
+
+/** fb_move_cursor:
+     *  Moves the cursor of the framebuffer to the given position
+     *
+     *  @param pos The new position of the cursor
+     */
+void fb_move_cursor(unsigned short pos)
+{
+    outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+    outb(FB_DATA_PORT, ((pos >> 8) & 0x00FF));
+    outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+    outb(FB_DATA_PORT, pos & 0x00FF);
+}
+
+/** fb_write_cell:
+     *  Writes a character with the given foreground and background to position i
+     *  in the framebuffer.
+     *
+     *  @param i  The location in the framebuffer
+     *  @param c  The character
+     *  @param fg The foreground color
+     *  @param bg The background color
+     */
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+{
+    fb[i] = c;
+    fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
+/**
+ fb_write function:
+ * @param buf content to write to framebuffer
+ * @param len length of content
+*/
+
+int fb_write(char *buf, unsigned int buf_len)
+{
+    unsigned int pos = 0;
+    for(unsigned int i= 0; i < buf_len; ++i){
+        pos = i * 2;
+        fb_write_cell(pos, buf[i], FB_GREEN, FB_DARK_GREY);
+    }
+        fb_move_cursor(buf_len);
+    return 0;
+}
--- bootloader/kmain.c
+++ bootloader/kmain.c
@@ -1,3 +1,15 @@
+#include "fb.c"
+
+unsigned int str_length(char * buf){
+    unsigned int count = 0;
+    while(buf[count]){
+        count+=1;
+    }
+    return count;
+}
+
 int kmain(){
-    return 105;
+    char * fb_dummy = "FRAMEBUFFER WORKING";
+    fb_write(fb_dummy, str_length(fb_dummy));
+    return 145;
 }
